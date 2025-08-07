ce709aabcf49ffe93e87c06783e3472bc9716b1a
Rabina4363sf <rabina.murugan@syncfusion.com>
2025-08-07 13:40:09 +0530

    KB-20838-Sample, README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# How-to-scroll-vertically-by-less-than-a-full-row-in-a-WPF-DataGrid
-How to scroll vertically by less than a full row in a WPF DataGrid (SfDataGrid)
+# How to scroll vertically by less than a full row in a WPF DataGrid (SfDataGrid)
+
+
+In [WPF DataGrid](https://www.syncfusion.com/wpf-controls/datagrid) (SfDataGrid), vertical scrolling moves one full row at a time. You can modify this behavior to scroll by your preferred units by adjusting the vertical offset  in the [ScrollChanged](https://learn.microsoft.com/en-us/dotnet/api/system.windows.controls.scrollviewer.scrollchanged?view=netframework-4.7.2) event. 
+
+**Code snippet to scroll vertically by less than a full row:**
+
+ 
+ ```csharp
+sfDataGrid.Loaded += OnLoaded;
+
+private void OnLoaded(object sender, RoutedEventArgs e)
+{
+    this.sfDataGrid.GetVisualContainer().ScrollOwner.ScrollChanged += OnScrollChanged;          
+}
+
+private void OnScrollChanged(object sender, ScrollChangedEventArgs e)
+{
+    if (e.VerticalChange == 24)
+     {
+        if (e.VerticalChange > 0)
+         {                    
+             // Here customize based on your scenario
+             double newOffset = e.VerticalOffset - 12;            
+             this.sfDataGrid.GetVisualContainer().SetVerticalOffset(newOffset);
+         }
+     }
+     if (e.VerticalChange == -24)
+      {
+         if (e.VerticalChange < 0)
+          {
+              // Here customize based on your scenario
+              double newOffset = e.VerticalOffset + 12;              
+            this.sfDataGrid.GetVisualContainer().SetVerticalOffset(newOffset);
+           }
+      }
+  } 
+ ```
+
+![VerticalScroll](VerticalScroll.Gif)
---
+++ SfDataGrid_Demo/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Syncfusion.UI.Xaml.Grid.Helpers;
+using Syncfusion.UI.Xaml.ScrollAxis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using  Syncfusion.UI.Xaml.ScrollAxis;
+using ScrollChangedEventArgs = System.Windows.Controls.ScrollChangedEventArgs;
+
+namespace SfDataGrid_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {      
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            sfDataGrid.Loaded += OnLoaded;            
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.sfDataGrid.GetVisualContainer().ScrollOwner.ScrollChanged += OnScrollChanged;          
+        }
+      
+        private void OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.VerticalChange == 24)
+            {
+                if (e.VerticalChange > 0)
+                {                    
+                    // Here customize based on your scenario
+                    double newOffset = e.VerticalOffset - 12;
+                    this.sfDataGrid.GetVisualContainer().SetVerticalOffset(newOffset);
+                }
+            }
+            if (e.VerticalChange == -24)
+            {
+                if (e.VerticalChange < 0)
+                {
+                    // // Here customize based on your scenario
+                    double newOffset = e.VerticalOffset + 12;
+                    this.sfDataGrid.GetVisualContainer().SetVerticalOffset(newOffset);
+                }
+            }
+        }
+    }
+}
